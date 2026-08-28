9ec8ef0597cf260471d6fec462b42a0bf145b805
Carlos Moron <carlosdmoron1225@gmail.com>
2026-08-27 21:26:07 -0500

    ajustar import de StatusBadge para solucionar build error

--- app/[lang]/dashboard-admin/trabajos/TrabajosPageClient.tsx
+++ app/[lang]/dashboard-admin/trabajos/TrabajosPageClient.tsx
@@ -1,9 +1,6 @@
-"use client";
-
-import { useState } from "react";
 import AdminSection from "../../../components/admin/AdminSection";
 import DataTable from "../../../components/admin/DataTable";
-import StatusBadge from "../../../components/admin/StatusBadge";
+import { StatusBadge } from "../../../components/admin/ui";
 import Modal from "../../../components/dashboard/Modal";
 import { useToast } from "../../../components/admin/Toast";
 import type { TableRow } from "../../../components/admin/DataTable";
